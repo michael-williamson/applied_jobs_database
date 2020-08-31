c7fb8dce3ba15ce957f036ead49f075da48a244a
michael-williamson <michaeldavid1721@yahoo.com>
2020-08-31 12:32:04 -0500

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Router, Route, Switch,Redirect} from 'react-router-dom';
+import history from './history';
 import './App.css';
 
 import FormMain from './components/formComponents/FormMain';
@@ -10,8 +12,10 @@ function App() {
       <div className="ui header">
         <h1>Job Database</h1>
       </div>
-      <FormMain/>
-      <TableMain/>
+      <Router history={history}>
+        <Route path="/form" component={FormMain}/>
+        <Route path="/table" component={TableMain}/>
+      </Router>
     </div>
   );
 }
