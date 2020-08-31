8dac0b6a9af2ec47da68683d959c1f57f43c51ef
michael-williamson <michaeldavid1721@yahoo.com>
2020-08-31 11:20:01 -0500

    added fields to form main

--- client/src/components/formComponents/FormMain.js
+++ client/src/components/formComponents/FormMain.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import {Field,reduxForm} from 'redux-form';
+//helper component
+import {ListOfStates} from './helperComponents/ListOfStates';
 
 class FormMain extends Component {
     render() {
         return (
-            <div>
-                FormMain
-            </div>
+            <form className="ui form">
+                <div className="field">
+                    <label>Company:</label>
+                    <Field name="company" component="input"/>
+                </div>
+                <div className="field">
+                    <label>City:</label>
+                    <Field name="city" component="input"/>
+                </div>
+                <div className="field">
+                    <label>State:</label>
+                    <Field name="state" component="select">
+                        <ListOfStates/>
+                    </Field>
+                </div>
+                <div className="field">
+                    <label>Type of Development:</label>
+                    <Field name="typeOfDevelopment" component="select">
+                        <option/>
+                        <option value="Front End Developer">Front End Devleoper</option>
+                        <option value="React Developer">React Developer</option>
+                        <option value="Full Stack Developer">Full Stack Developer</option>
+                    </Field>
+                </div>
+            </form>
         );
     }
 }
 
-export default FormMain;
+export default reduxForm({form:'jobForm'})(FormMain);
